6c2a0b89be96f70e0f06c27946cf55bc0ee0e7eb
Crazyshooting <66379820+Crazyshooting@users.noreply.github.com>
2020-09-21 21:15:19 +0300

    Create give.js

---
+++ src/commands/give.js
@@ -0,0 +1,50 @@
+module.exports.run = async (bot, message, args) => {
+    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(member => member.user.username === args.slice(0).join(' ') || member.user.username === args[0]);
+    const userData = await bot.fetchUser(member.user.id);
+    const authorData = await bot.fetchUser(message.author.id);
+
+    if (!member) return message.channel.send(`Who are you giving the coins to?`);
+
+    if (!args[1]) return message.channel.send(`How much coins are you giving them?`);
+
+    if (isNaN(args[1]) && args[1] !== 'all') return message.channel.send(`Thats not a valid option`)
+ 
+    if (args[1] == 'all') {
+        const toGive = authorData.coinsInWallet;
+
+        authorData.coinsInWallet = 0;
+
+        await authorData.save();
+
+        userData.coinsInWallet = (userData.coinsInWallet + toGive);
+
+        userData.save();
+
+        message.channel.send(`You gave ${member} **${toGive}** coins`); //Change the message how u like
+    } else {
+        const toGive = args[1];
+
+        if (toGive > authorData.coinsInWallet) return message.reply(`You don't have that much coins`);
+
+        authorData.coinsInWallet = (authorData.coinsInWallet - parseInt(toGive));
+
+        await authorData.save();
+
+        userData.coinsInWallet = (userData.coinsInWallet + parseInt(toGive));
+
+        await userData.save();
+
+        message.channel.send(`You gave ${member} **${toGive}** coins`); //Change the message how u like
+    }
+
+}
+module.exports.config = {
+    name: 'give', // Command Name
+    description: 'give user coins.', // Description
+    usage: 'air give <amount | all>', // Usage
+    botPerms: [], // Bot permissions needed to run command. Leave empty if nothing.
+    userPerms: [], // User permissions needed to run command. Leave empty if nothing.
+    aliases: [], // Aliases 
+    bankSpace: 0, // Amount of bank space to give when command is used.
+    cooldown: 10 // Command Cooldown
+}
