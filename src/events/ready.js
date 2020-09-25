917bcf0c03abf0178c1b59a008db2932ea6a6bb9
Gavin Vu <62964708+Gav-King@users.noreply.github.com>
2020-09-25 09:08:43 -0400

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,5 @@
 module.exports = bot => {
     bot.user.setActivity('air help', { type: 'PLAYING' });
     console.log(`${bot.user.tag} is online.`);
+    await bot.economy.updateMany({}, { passive: false });
 }
