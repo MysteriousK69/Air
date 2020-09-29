e8cbb0404cfcaa1776d2deeb0393f5f75191b58a
MysteriousK69 <63832146+MysteriousK69@users.noreply.github.com>
2020-09-29 19:23:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const { Collection } = require('discord.js');
 const MongoClient = require('./utils/MongoClient');
 const bot = new MongoClient();
-
+// contribooshun
+// me is paro in NodeJS
+// C:
+// dis bot sexy
 bot.login(process.env.TOKEN);
 
 bot.commands = new Collection();
